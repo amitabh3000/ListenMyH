b7ff35aec124df990463236c93e666eeaecc0e4d
amitabhs <amitab11@gmail.com>
2015-06-11 14:16:04 +0530

    uykouil

--- ListenMy/src/com/amitabh/listenmy/MainActivity.java
+++ ListenMy/src/com/amitabh/listenmy/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends Activity implements OnClickListener,
 	}
 
 	private void setAllListener() {
-		int i = 0;
+		int i = 0;//klj
 		next.setOnClickListener(this);
 		previous.setOnClickListener(this);
 		playPause.setOnCheckedChangeListener(this);
